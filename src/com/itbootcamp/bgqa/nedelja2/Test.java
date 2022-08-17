36c79b51862adea22290ddf61794ac5e50ba76b7
Marko Stankovic <marko.stankovic@boohoo.com>
2022-08-17 11:23:03 +0200

    zadaci 2

--- src/com/itbootcamp/bgqa/nedelja2/Test.java
+++ src/com/itbootcamp/bgqa/nedelja2/Test.java
@@ -4,20 +4,10 @@ import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
+        Scanner rec = new Scanner(System.in);
 
-        int brojPonavljanja = 0;
-        String trazenaRec = "cool";
-        for (int i = 0; true; i++) {
-            System.out.println("Unesite string koji sadrzi bar jednom 'cool': ");
-            String text = s.nextLine().toLowerCase();
-            if (text.contains(trazenaRec)) {
-                brojPonavljanja++;
-            } else if (text.contains("cao")) {
-                System.out.println("Program je prekinut!");
-                break;
-            }
-        }
-        System.out.println("Broj ponavljanja reci 'cool' je: " + brojPonavljanja);
+
+        String input = rec.next(".*+");
+        System.out.println(input);
     }
 }
