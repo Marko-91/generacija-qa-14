c0c98f50911730455279bd2f90cf56d067656f6d
Marko Stankovic <marko.stankovic@boohoo.com>
2022-09-15 11:56:45 +0200

    helper random

--- src/com/itbootcamp/bgqa/nedelja3/Dan2Metode.java
+++ src/com/itbootcamp/bgqa/nedelja3/Dan2Metode.java
@@ -2,9 +2,9 @@ package com.itbootcamp.bgqa.nedelja3;
 
 public class Dan2Metode {
     public static void main(String[] args) {
-        System.out.println(stepen(-2, -3));
+        //System.out.println(stepen(-2, -3));
         for (int i = 0; i < 100; i++) {
-            System.out.println(random(5.0, 10.0));
+            System.out.println(random(5.5, 7.5));
         }
     }
 
@@ -12,9 +12,8 @@ public class Dan2Metode {
         return (int) (Math.random() * (gornjaGranica - donjaGranica + 1)) + donjaGranica;
     }
 
-    public static double random(double m, double n) {
-        double nasumicniBroj= Math.min(m,n)+Math.random()*Math.max(m,n);
-        return nasumicniBroj;
+    public static double random(double donjaGranica, double gornjaGranica) {
+        return Math.round((Math.random() * (gornjaGranica - donjaGranica + 1) + donjaGranica) * 100.0 ) / 100.0;
     }
 
     public static double stepen(double n, int eksp) {
