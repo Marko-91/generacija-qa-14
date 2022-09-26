f43cabd1f5d21138fef7fa5d39a1bdf09f5f9f40
Marko Stankovic <mare_rocknrola@hotmail.com>
2022-09-26 22:10:18 +0200

    pom v2

--- src/com/itbootcamp/bgqa/nedelja7/automation/ui/shopping/pages/HomePage.java
+++ src/com/itbootcamp/bgqa/nedelja7/automation/ui/shopping/pages/HomePage.java
@@ -1,8 +1,10 @@
 package com.itbootcamp.bgqa.nedelja7.automation.ui.shopping.pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -14,6 +16,9 @@ public class HomePage {
     private WebDriver webDriver;
     private final String MODAL_HEADING_XPATH = "/html[1]/body[1]/section[2]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/h4[1]";
     private final String MODAL_HEADING_TEXT = "Added!";
+    private final String HOME_PAGE = "/html[1]/body[1]/header[1]/div[1]/div[1]/div[1]/div[2]/div[1]/ul[1]/li[1]/a[1]";
+    private final String ADD_TO_CART = "/html[1]/body[1]/section[2]/div[1]/div[1]/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/a[1]";
+    private final String VIEW_CART_BTN = "/html[1]/body[1]/section[2]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[2]/p[2]/a[1]";
 
     public HomePage(WebDriver webDriver) {
         this.webDriver = webDriver;
@@ -27,27 +32,28 @@ public class HomePage {
         return addedText.getText();
     }
 
-    public void clickOnHome() {
-
+    public void clickOnHome(){
+        WebElement home = webDriver.findElement(By.xpath(HOME_PAGE));
+        home.click();
     }
-
+    private WebElement getDress(int dressNumber) {
+        return webDriver.findElement(By.xpath("/html[1]/body[1]/section[2]/div[1]/div[1]/div[2]/div[1]/div[" + dressNumber + "]"));
+    }
     public void scrollToDress(int dressNumber) {
-
+        JavascriptExecutor javascriptExecutor = (JavascriptExecutor) webDriver;
+        javascriptExecutor.executeScript("arguments[0].scrollIntoView(true)", getDress(dressNumber));
     }
-
-    public void hoverToDress(WebElement dress) {
-
+    public void hoverToDress(int dressNumber) {
+        Actions actions = new Actions(webDriver);
+        actions.moveToElement(getDress(dressNumber)).perform();
     }
-
-    public void clickToAddCardButton() {
-
+    public void clickToAddCartButton() {
+        WebElement addToCard = webDriver.findElement(By.xpath(ADD_TO_CART));
+        addToCard.click();
     }
-
-    public void clickOnViewCardButton() {
-
+    public void clickToViewCartButton() {
+        WebElement viewCart = webDriver.findElement(By.xpath(VIEW_CART_BTN));
+        viewCart.click();
     }
-    ///html[1]/body[1]/section[2]/div[1]/div[1]/div[2]/div[1]/div[9]
-    ///html[1]/body[1]/section[2]/div[1]/div[1]/div[2]/div[1]/div[10]
-    ///html[1]/body[1]/section[2]/div[1]/div[1]/div[2]/div[1]/div[11]
 
 }
--- src/com/itbootcamp/bgqa/nedelja7/automation/ui/shopping/tests/TestHomePage.java
+++ src/com/itbootcamp/bgqa/nedelja7/automation/ui/shopping/tests/TestHomePage.java
@@ -38,45 +38,16 @@ public class TestHomePage {
         wd.get("https://www.automationexercise.com/");
         wd.manage().window().maximize();
         HomePage homePage = new HomePage(wd);
-
-        //region Idem na home
-        WebElement home = wd.findElement(By.xpath("/html[1]/body[1]/header[1]/div[1]/div[1]/div[1]/div[2]/div[1]/ul[1]/li[1]/a[1]"));
-        home.click();
-        //homePage.clickOnHome();
-        //endregion
-
-        //region Scroll do prve haljine
-        WebElement prvaHaljina = wd.findElement(By.xpath("/html[1]/body[1]/section[2]/div[1]/div[1]/div[2]/div[1]/div[2]"));
-        JavascriptExecutor javascriptExecutor = (JavascriptExecutor) wd;
-        javascriptExecutor.executeScript("arguments[0].scrollIntoView(true)", prvaHaljina);
-        //homePage.scrollToDress(2);
-        //endregion
-
-        //region Hover na tu haljinu
-        Actions actions = new Actions(wd);
-        actions.moveToElement(prvaHaljina).perform();
-        //endregion
-
-        //region  Kliknemo 'add to card'
-        WebElement addToCard = wd.findElement(By.xpath("/html[1]/body[1]/section[2]/div[1]/div[1]/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/a[1]"));
-        addToCard.click();
-        //endregion
-
-        //region Potvrdimo da se pojavilo 'Added'
+        homePage.clickOnHome();
+        homePage.scrollToDress(2);
+        homePage.hoverToDress(2);
+        homePage.clickToAddCartButton();
         Assert.assertEquals(homePage.getModalTitle(), MODAL_TITLE_EXPECTED, "The text should be added");
-        //endregion
-
-        //region Kliknemo view cart
-        WebElement viewCart = wd.findElement(By.xpath("/html[1]/body[1]/section[2]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[2]/p[2]/a[1]"));
-        viewCart.click();
-        //endregion
-
-        //region Potvrdimo da je haljina u korpi
+        homePage.clickToViewCartButton();
         WebElement slika = wd.findElement(By.xpath("/html[1]/body[1]/section[1]/div[1]/div[2]/table[1]/tbody[1]/tr[1]/td[1]/a[1]/img[1]"));
         String actualUrl = slika.getAttribute("src");
         Assert.assertEquals(actualUrl, "https://www.automationexercise.com/get_product_picture/1", "the url should be 'get_product_picture/1'");
         //endregion
-
         wd.close();
     }
 
