8a9135debe5b6e469e73f983feeb6058e4a53122
Marko Stankovic <mare_rocknrola@hotmail.com>
2022-09-27 21:39:52 +0200

    test e2e tegister

--- src/com/itbootcamp/bgqa/nedelja7/automation/ui/shopping/pages/SignUp.java
+++ src/com/itbootcamp/bgqa/nedelja7/automation/ui/shopping/pages/SignUp.java
@@ -1,7 +1,9 @@
 package com.itbootcamp.bgqa.nedelja7.automation.ui.shopping.pages;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
@@ -28,6 +30,33 @@ public class SignUp {
 
     @FindBy(id = "years")
     private WebElement year;
+
+    @FindBy(id = "first_name")
+    private WebElement firstName;
+
+    @FindBy(id = "last_name")
+    private WebElement lastName;
+
+    @FindBy(id = "address1")
+    private WebElement address1;
+
+    @FindBy(id = "country")
+    private WebElement country;
+
+    @FindBy(id = "state")
+    private WebElement state;
+
+    @FindBy(id = "city")
+    private WebElement city;
+
+    @FindBy(id = "zipcode")
+    private WebElement zipcode;
+
+    @FindBy(id = "mobile_number")
+    private WebElement mobileNumber;
+
+    @FindBy(xpath = "/html[1]/body[1]/section[1]/div[1]/div[1]/div[1]/div[1]/form[1]/button[1]")
+    private WebElement createAccount;
 
     public SignUp(WebDriver webDriver) {
         this.webDriver = webDriver;
@@ -65,5 +94,44 @@ public class SignUp {
     public void selectYear(int year) {
         Select dropdown = new Select(this.year);
         dropdown.selectByValue(String.valueOf(year));
+    }
+
+    public void inputFirstName(String firstName) {
+        this.firstName.sendKeys(firstName);
+    }
+
+    public void inputLastName(String lastName) {
+        this.lastName.sendKeys(lastName);
+    }
+
+    public void inputAddress(String address) {
+        this.address1.sendKeys(address);
+    }
+
+    public void selectCountry(String country) {
+        Select dropdown = new Select(this.country);
+        dropdown.selectByVisibleText(country);
+    }
+
+    public void inputState(String state) {
+        this.state.sendKeys(state);
+    }
+
+    public void inputCity(String city) {
+        this.city.sendKeys(city);
+    }
+
+    public void inputZipcode(String zipcode) {
+        this.zipcode.sendKeys(zipcode);
+    }
+
+    public void inputMobile(String number) {
+        this.mobileNumber.sendKeys(number);
+    }
+
+    public void createAccount() {
+        JavascriptExecutor javascriptExecutor = (JavascriptExecutor) webDriver;
+        javascriptExecutor.executeScript("arguments[0].scrollIntoView(true)", this.createAccount);
+        this.createAccount.click();
     }
 }
