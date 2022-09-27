24488a3ce513666f33d0ccc00a65c6711fda11f6
Marko Stankovic <mare_rocknrola@hotmail.com>
2022-09-27 21:50:29 +0200

    parametri

--- src/com/itbootcamp/bgqa/nedelja7/automation/ui/shopping/tests/TestHomePage.java
+++ src/com/itbootcamp/bgqa/nedelja7/automation/ui/shopping/tests/TestHomePage.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 
@@ -69,6 +70,13 @@ public class TestHomePage {
 
         Assert.assertEquals(viewCart.getFirstSrcAttr(), "https://www.automationexercise.com/get_product_picture/1", "the url should be 'get_product_picture/1'");
     }
+
+    @Parameters({"test"})
+    @Test
+    public void testParams(String parameter) {
+        System.out.println(parameter);
+    }
+
 
     @Test
     public void testAddToShoppingCardUnhappyPath() {
